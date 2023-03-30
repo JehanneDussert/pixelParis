fb9e07ebe6e9488d939db31b976b7283153ec963
Jehanne DUSSERT <jdussert@student.42.fr>
2023-03-30 08:01:36 +0200

    team page

--- src/pages/Team/index.jsx
+++ src/pages/Team/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { DivContainer, BoxContainer, DivPage } from "../../styles/Atom";
+import Image from "../../assets/building.jpg"
 
 function Team() {
     return (
         <DivPage>
             <DivContainer id="team">
-                <BoxContainer>Nobila</BoxContainer>
-                <BoxContainer>Jehanne</BoxContainer>
+                <BoxContainer>
+                    <h2 class="title">Nobila Traore</h2>
+                    <img class="profile" src={Image} width="300px"/>
+                    <p class="subtitle"> Développeur C++ / Unreal Engine 5</p>
+                </BoxContainer>
+                <BoxContainer>
+                    <h2 class="title">Jehanne Dussert</h2>
+                    <img class="profile" src={Image} width="300px"/>
+                    <p class="subtitle"> Développeuse C++ / Unreal Engine 5</p>
+                </BoxContainer>
             </DivContainer>
         </DivPage>
     )
